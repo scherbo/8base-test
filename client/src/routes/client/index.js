e3be5b8c8a12a1048c51fae8e35f9fe3b6f01956
Sergey Scherbo <scherbo@Mac-mini-Sergey.local>
2019-10-08 15:43:30 +0300

    client refactor

--- client/src/routes/client/index.js
+++ client/src/routes/client/index.js
@@ -2,65 +2,57 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import * as R from 'ramda';
-import { Card, Loader, Heading, Label, Paragraph, Table } from '@8base/boost'
-import styled from '@emotion/styled'
+import { Card, Loader, Heading, Label, Paragraph, Table } from '@8base/boost';
 
 import * as sharedGraphQL from 'shared/graphql';
-
-const ClientInfoSpacer = styled.div`
-  padding-top: 2rem;
-  padding-right: 2rem;
-`;
+import { InfoSpacer } from 'shared/styled';
 
 // for some reason useParams hook doesn't work
 // https://github.com/ReactTraining/react-router/issues/6944
 export const Client = withRouter(({ match }) => {
   const { loading, error, data } = useQuery(sharedGraphQL.CLIENT_QUERY, {
     variables: { id: match.params.id },
-  })
+  });
 
   const fields = [
-    { hidden: 'firstName', label: 'First Name' },
-    { hidden: 'lastName', label: 'Last Name'},
-    { hidden: 'email', label: 'Email' },
-    { hidden: 'phone', label: 'Phone' },
-    { hidden: 'birthday', label: 'Birthday' },
-  ]
+    { path: ['firstName'], label: 'First Name' },
+    { path: ['lastName'], label: 'Last Name' },
+    { path: ['email'], label: 'Email' },
+    { path: ['phone'], label: 'Phone' },
+    { path: ['birthday'], label: 'Birthday' },
+  ];
 
-  if (loading) return (
-    <Card stretch>
-      <Loader stretch />
-    </Card>
-  )
+  if (loading)
+    return (
+      <Card stretch>
+        <Loader stretch />
+      </Card>
+    );
 
-  if (error) return (
-    <Card stretch>
-      <Card.Body>
-        {error.message}
-      </Card.Body>
-    </Card>
-  )
-
-  console.log('DATA', data)
-  console.log(R.pathOr([], ['orders', 'items'], data.client))
+  if (error)
+    return (
+      <Card stretch>
+        <Card.Body>{error.message}</Card.Body>
+      </Card>
+    );
 
   return (
     <Card stretch>
       <Card.Header>
-        <Heading type='h4'>Client ID: {match.params.id}</Heading>
+        <Heading type="h4">Client ID: {match.params.id}</Heading>
       </Card.Header>
       <Card.Body>
-        <Heading type='h4'>Client Info:</Heading>
+        <Heading type="h4">Client Info:</Heading>
         {fields.map(field => (
-          <ClientInfoSpacer>
-            <Label kind='secondary' text={field.label} />
-            <Paragraph>{data.client[field.hidden]}</Paragraph>
-          </ClientInfoSpacer>
+          <InfoSpacer>
+            <Label kind="secondary" text={field.label} />
+            <Paragraph>{R.pathOr([], field.path, data.client) || '-'}</Paragraph>
+          </InfoSpacer>
         ))}
       </Card.Body>
       <Card.Footer>
-        <Heading type='h4'>Client Orders:</Heading>
-        <ClientInfoSpacer>
+        <Heading type="h4">Client Orders:</Heading>
+        <InfoSpacer>
           <Table>
             <Table.Header columns="repeat(4, 1fr) 60px">
               <Table.HeaderCell>Address</Table.HeaderCell>
@@ -68,22 +60,20 @@ export const Client = withRouter(({ match }) => {
               <Table.HeaderCell>Comment</Table.HeaderCell>
               <Table.HeaderCell>Status</Table.HeaderCell>
             </Table.Header>
-            <Table.Body data={ R.pathOr([], ['orders', 'items'], data.client) }>
-              {
-                (order) => (
-                  <Table.BodyRow columns="repeat(4, 1fr) 60px" key={order.id}>
-                    {console.log('ORDER', order)}
-                    <Table.BodyCell>{ R.pathOr('Unititled', ['address'], order) }</Table.BodyCell>
-                    <Table.BodyCell>{ R.pathOr('Unititled', ['deliveryDt'], order) }</Table.BodyCell>
-                    <Table.BodyCell>{ R.pathOr('Unititled', ['comment'], order) }</Table.BodyCell>
-                    <Table.BodyCell>{ R.pathOr('Unititled', ['status'], order) }</Table.BodyCell>
-                  </Table.BodyRow>
-                )
-              }
+            <Table.Body data={R.pathOr([], ['orders', 'items'], data.client)}>
+              {order => (
+                <Table.BodyRow columns="repeat(4, 1fr) 60px" key={order.id}>
+                  {console.log('ORDER', order)}
+                  <Table.BodyCell>{R.pathOr('Unititled', ['address'], order)}</Table.BodyCell>
+                  <Table.BodyCell>{R.pathOr('Unititled', ['deliveryDt'], order)}</Table.BodyCell>
+                  <Table.BodyCell>{R.pathOr('Unititled', ['comment'], order)}</Table.BodyCell>
+                  <Table.BodyCell>{R.pathOr('Unititled', ['status'], order)}</Table.BodyCell>
+                </Table.BodyRow>
+              )}
             </Table.Body>
           </Table>
-        </ClientInfoSpacer>
+        </InfoSpacer>
       </Card.Footer>
     </Card>
-  )
+  );
 });
