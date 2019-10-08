9849970fa697e42bbd07b196f412d440be1b1af9
Sergey Scherbo <scherbo@Mac-mini-Sergey.local>
2019-10-08 16:49:43 +0300

    change button text

--- client/src/routes/clients/ClientEditDialog.js
+++ client/src/routes/clients/ClientEditDialog.js
@@ -11,8 +11,8 @@ const CLIENT_EDIT_DIALOG_ID = 'CLIENT_EDIT_DIALOG_ID';
 class ClientEditDialog extends React.Component {
   static contextType = ModalContext;
 
-  onCreateSubmit = id => async (data) => {
-    await this.props.clientUpdate({ variables: { data: { ...data, id } }});
+  onCreateSubmit = id => async data => {
+    await this.props.clientUpdate({ variables: { data: { ...data, id } } });
 
     this.context.closeModal(CLIENT_EDIT_DIALOG_ID);
   };
@@ -22,68 +22,55 @@ class ClientEditDialog extends React.Component {
   };
 
   renderFormContent = ({ handleSubmit, invalid, submitting, pristine }) => (
-    <form onSubmit={ handleSubmit }>
-      <Dialog.Header title="Edit Client" onClose={ this.onClose } />
+    <form onSubmit={handleSubmit}>
+      <Dialog.Header title="Edit Client" onClose={this.onClose} />
       <Dialog.Body scrollable>
         <Grid.Layout gap="sm" stretch>
           <Grid.Box>
-            <Field
-              name="firstName"
-              label="First name"
-              component={ InputField }
-            />
+            <Field name="firstName" label="First name" component={InputField} />
           </Grid.Box>
           <Grid.Box>
-            <Field
-              name="lastName"
-              label="Last name"
-              component={ InputField }
-            />
+            <Field name="lastName" label="Last name" component={InputField} />
           </Grid.Box>
           <Grid.Box>
-            <Field
-              name="email"
-              label="Email"
-              component={ InputField }
-            />
+            <Field name="email" label="Email" component={InputField} />
           </Grid.Box>
           <Grid.Box>
-            <Field
-              name="phone"
-              label="Phone"
-              component={ InputField }
-              mask="+7 (999) 999-99-99"
-            />
+            <Field name="phone" label="Phone" component={InputField} mask="+7 (999) 999-99-99" />
           </Grid.Box>
           <Grid.Box>
-            <Field
-              name="birthday"
-              label="Birthday"
-              component={ InputField }
-              mask="9999-99-99"
-            />
+            <Field name="birthday" label="Birthday" component={InputField} mask="9999-99-99" />
           </Grid.Box>
         </Grid.Layout>
       </Dialog.Body>
       <Dialog.Footer>
-        <Button color="neutral" variant="outlined" disabled={ submitting } onClick={ this.onClose }>Cancel</Button>
-        <Button color="primary" type="submit" loading={ submitting }>Create Client</Button>
+        <Button color="neutral" variant="outlined" disabled={submitting} onClick={this.onClose}>
+          Cancel
+        </Button>
+        <Button color="primary" type="submit" loading={submitting}>
+          Save Changes
+        </Button>
       </Dialog.Footer>
     </form>
   );
 
   renderForm = ({ args }) => {
     return (
-      <Form type="UPDATE" tableSchemaName="Clients" onSubmit={ this.onCreateSubmit(args.initialValues.id) } initialValues={args.initialValues}>
-        { this.renderFormContent }
+      <Form
+        type="UPDATE"
+        tableSchemaName="Clients"
+        onSubmit={this.onCreateSubmit(args.initialValues.id)}
+        initialValues={args.initialValues}
+      >
+        {this.renderFormContent}
       </Form>
-    )
-  }
+    );
+  };
 
   render() {
     return (
-      <Dialog id={ CLIENT_EDIT_DIALOG_ID } size="sm">
-        { this.renderForm }
+      <Dialog id={CLIENT_EDIT_DIALOG_ID} size="sm">
+        {this.renderForm}
       </Dialog>
     );
   }
@@ -94,7 +81,7 @@ ClientEditDialog = graphql(sharedGraphQL.CLIENT_UPDATE_MUTATION, {
   options: {
     refetchQueries: ['ClientsList'],
     context: {
-      [TOAST_SUCCESS_MESSAGE]: 'Client successfuly update'
+      [TOAST_SUCCESS_MESSAGE]: 'Client successfuly update',
     },
   },
 })(ClientEditDialog);
